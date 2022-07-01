01901d42f86af6da2cd693607269ea0779d9861e
Sonlowami <uwilowami64@outlook.com>
2022-07-01 02:37:56 -0700

    Add documentation for main method

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /** Main function is the entry into the program.
+ *
+ * Return: integer 0 if successful
 */
 int main(void)
 {
 	printf ("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	return (0);
 }
