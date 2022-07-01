85a33eed88863537c52cfe858c4af5e745329554
Sonlowami <uwilowami64@outlook.com>
2022-07-01 12:55:31 -0700

    Add tabs to indent lines

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,12 @@
 */
 int main(void)
 {
-printf("%s: %d %s\n", "size of a char", sizeof(char), "byte(s)");
-printf("%s: %d %s\n", "size of an int", sizeof(int), "byte(s)");
-printf("%s: %d %s\n", "size of a long int", sizeof(long int), "byte(s)");
-printf("%s: %d %s\n", "size of a long long int",
-sizeof(long long int), "byte(s)");
-printf("%s: %d %s\n", "size of a float", sizeof(float), "byte(s)");
+	printf("%s: %d %s\n", "size of a char", sizeof(char), "byte(s)");
+	printf("%s: %d %s\n", "size of an int", sizeof(int), "byte(s)");
+	printf("%s: %d %s\n", "size of a long int", sizeof(long int), "byte(s)");
+	printf("%s: %d %s\n", "size of a long long int",
+	sizeof(long long int), "byte(s)");
+	printf("%s: %d %s\n", "size of a float", sizeof(float), "byte(s)");
 
 return (0);
 }
