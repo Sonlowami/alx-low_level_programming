520af844ceef381b8ae74602b4b19eac8e7d46f9
Sonlowami <uwilowami64@outlook.com>
2022-07-01 11:14:01 -0700

    Create file 101 that outputs to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - runtime entry point
+ *
+ *The program does
+ *Return: int 0, if successful
+*/
+int main(void)
+{
+  fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60, 1, stderr);
+  return (0);
+}
