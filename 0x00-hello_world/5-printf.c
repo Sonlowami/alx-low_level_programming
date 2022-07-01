ee6a400d10bb472ab724eaa5c7e8a58ba74d20d6
Sonlowami <uwilowami64@outlook.com>
2022-06-30 21:01:35 -0700

    betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+/** Main function is the entry into the program.
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf ("with proper grammar, but the outcome is a piece of art,");
 	return(0);
 }
