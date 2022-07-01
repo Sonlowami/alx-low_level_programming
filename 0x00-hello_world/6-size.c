c27409e9fd1cce8c926fb158b5c86d376701e220
Sonlowami <uwilowami64@outlook.com>
2022-07-01 05:32:31 -0700

    Fix documentation and styling errors in 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 */
 int main(void)
 {
-  printf("%s : %d %s\n", "size of a char  is", sizeof(char), "byte(s)");
-  printf("%s : %d %s\n", "size of a int is", sizeof(int), "byte(s)");
-  printf("%s : %d %s\n", "size of a long int is", sizeof(long int), "byte(s)");
-  printf("%s : %d %s\n", "size of a long long int is", sizeof(long long int), "byte(s)");
-  printf("%s : %d %s\n", "size of a float is", sizeof(float), "byte(s)");
+  printf("%s: %d %s\n", "size of a char", sizeof(char), "byte(s)");
+  printf("%s: %d %s\n", "size of a int", sizeof(int), "byte(s)");
+  printf("%s: %d %s\n", "size of a long int", sizeof(long int), "byte(s)");
+  printf("%s: %d %s\n", "size of a long long int", sizeof(long long int), "byte(s)");
+  printf("%s: %d %s\n", "size of a float", sizeof(float), "byte(s)");
 
   return (0);
 }
