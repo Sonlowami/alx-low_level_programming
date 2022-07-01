6b2aa25db077212f105a96c831090eef49bf6cbf
Sonlowami <uwilowami64@outlook.com>
2022-07-01 11:21:36 -0700

    Update code style to meet betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- *main - runtime entry point
- *
- *The program does
- *Return: int 0, if successful
+*main - runtime entry point
+*
+*The program does
+*Return: int 0, if successful
 */
 int main(void)
 {
-  fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60, 1, stderr);
-  return (0);
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+60, 1, stderr);
+return (0);
 }
