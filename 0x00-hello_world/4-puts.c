f8c114309e533f42d4995244c0af1cd6acb2f2ba
Sonlowami <uwilowami64@outlook.com>
2022-07-01 02:20:00 -0700

    Add a documentation to main method

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - serves as entry point
+ *
+ * The program prints just a single line of text
+ * section header: standard input and output from the standard library
+ * return: 0 if successful
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("%s", "\"Programming is like building a multilingual puzzle");
 	return (0);
 }
