5821dcf169374a93843e98e4d174dd5fa1aef2b3
Sonlowami <uwilowami64@outlook.com>
2022-07-01 06:00:42 -0700

    Attempt to erase space indentation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,6 @@ int main(void)
   printf("%s: %d %s\n", "size of a long int", sizeof(long int), "byte(s)");
   printf("%s: %d %s\n", "size of a long long int", sizeof(long long int), "byte(s)");
   printf("%s: %d %s\n", "size of a float", sizeof(float), "byte(s)");
+
   return (0);
 }
