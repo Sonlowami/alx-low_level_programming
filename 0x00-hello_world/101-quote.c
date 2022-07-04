52deeacb157efb9ccf5bb5b457b6e60d8c895f2d
Sonlowami <uwilowami64@outlook.com>
2022-07-04 05:19:57 -0700

    Change return value to 1.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 *main - runtime entry point
 *
 *The program does
-*Return: int 0, if successful
+*Return: int 1, if successful
 */
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-60, 1, stderr);
-return (0);
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19",
+59, 1, stderr);
+return (1);
 }
