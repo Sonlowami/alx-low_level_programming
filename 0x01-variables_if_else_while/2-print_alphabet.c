f8b69a67f63fd0caee715f7ba3b67d8b02076a1a
Sonlowami <uwilowami64@outlook.com>
2022-07-05 13:05:08 -0700

    Print all leters of alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all letters of alphabet in lower case
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+		putchar(ch);
+	putchar('\n');
+}
