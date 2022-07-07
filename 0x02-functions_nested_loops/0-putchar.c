f8d292d00677f37d03c6771ba3e200566eba1e0b
Sonlowami <uwilowami64@outlook.com>
2022-07-07 10:41:40 -0700

    Create README.md and the first program file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the program prints _putchar to standard output
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This directory contain a bunch of programs I have written in C in compliance
+with my educational work at ALX Software Engineering School. 
