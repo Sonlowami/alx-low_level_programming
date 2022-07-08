197cbec7b92282e2a3447bc87d52744bb4a1b93a
Sonlowami <uwilowami64@outlook.com>
2022-07-08 10:00:33 -0700

    Add a function tocheck the sign of an integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <main.h>
+
+/**
+ * print_sign - checks if the number is positive, negative or 0 and
+ * print it's sign
+ * @n: an integer to check the sign against
+ *
+ * Return: 1 if positive
+ *	0 if 0
+ *	-1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
