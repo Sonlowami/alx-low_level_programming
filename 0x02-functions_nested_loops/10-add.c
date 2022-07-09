5f3cd8e37f84243dd7712d1c6fe70821495fa6b2
Sonlowami <uwilowami64@outlook.com>
2022-07-09 08:20:08 -0700

    Add a function to add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * add - adds two integers and return the result
+ * @x: the first operand
+ * @y: the second operand
+ * Return: integer summary of x and y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
