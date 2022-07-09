e7208687703922ce4b4dbec2cf4e57dd59230d64
Sonlowami <uwilowami64@outlook.com>
2022-07-09 00:17:37 -0700

    Fixing bugs with multiple conditions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,16 +6,34 @@
 
 void jack_bauer(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j < 60; j++)
+		for (j = 0; j < 6; j++)
 		{
-			_putchar('0' + i);
-			_putchar(':');
-			_putchar('0' + j);
-			_putchar('\n');
+			for (k = 0; k < 10; k++)
+			{
+				if (i < 10)
+				{
+					_putchar('0');
+					_putchar('0' + i);
+				}
+				else if (i < 20 && i > 10)
+				{
+					_putchar('1');
+					_putchar('0' + (x % 10));
+				}
+				else
+				{
+					_putchar('2');
+					_putchar('0' + (x % 10));
+				}
+				_putchar(':');
+				_putchar('0' + j);
+				_putchar('0' + k);
+				_putchar('\n');
+			}
 		}
 	}
 }
