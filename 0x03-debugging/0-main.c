22148398c878bc4e11ab573da12076aeb7435217
Sonlowami <uwilowami64@outlook.com>
2022-07-09 23:31:23 -0700

    Limit the code to 16 lines

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-
 
 /**
  * main - tests function that prints if integer is positive or negative
@@ -11,17 +6,11 @@
  * Return: 0
  */
 
-
-
 int main(void)
-
 {
-
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
-
 	return (0);
 }
