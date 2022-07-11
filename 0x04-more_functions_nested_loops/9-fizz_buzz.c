73162c2d4ffd6e2c3b6c464b8b57ba886e480ae0
Sonlowami <uwilowami64@outlook.com>
2022-07-11 14:24:49 -0700

    Solve fizzBuzz programming challenge

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+
+#include "stdio.h"
+
+/**
+ * main - print numbers from 1 to 100
+ * print Fizz for multiplesof 3
+ * print Buzz for multiplesof 5
+ * print FizzBuzz for multiples of both 3 and 5
+ *
+ * Return: int 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		printf("%c", ' ');
+	}
+	printf("%c", '\n');
+	return (0);
+}
