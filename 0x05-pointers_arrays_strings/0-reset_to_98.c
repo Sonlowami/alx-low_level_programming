3f960d1c509c33a3857c08e33d8a6172c28236ce
Sonlowami <uwilowami64@outlook.com>
2022-07-13 06:51:13 -0700

    Add a function that resets the value in the calling function to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+/**
+ * reset_to_98 - change the value a pointer points to to 98
+ * @n: the pointer to an integer to be changed.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void reset_to_98(int *n);
+
+int _putchar(char c);
