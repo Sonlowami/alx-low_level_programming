b9e52c0f9f44ddcbd06ca805711444f804790248
Sonlowami <uwilowami64@outlook.com>
2022-07-17 12:38:40 -0700

    Change encoding logic to use 1 if

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,21 +7,18 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; *(str+i); i++)
+	char o_code[10] = {'a', 'A', 'e', 'E', 'O', 'o', 'L', 'l', 't', 'T'};
+	char leet_code[10] = {'4', '4', '3', '3', '0', '0', '1', '1', '7', '7'};
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (*(str + i) == 65 || *(str + i) == 97)
-			*(str + i) = 52;
-		else if (*(str + i) == 101 || *(str + i) == 69)
-			*(str + i) = 51;
-		else if (*(str + i) == 111 || *(str + i) == 79)
-			*(str + i) = 50;
-		else if (*(str + i) == 84 || *(str + i) == 116)
-			*(str + i) = 55;
-
-		else if (*(str + i) == 108 || *(str + i) == 76)
-			*(str + i) = 52;
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == o_code[j])
+				str[i] = leet_code[j];
+		}
 	}
 	return (str);
 }
