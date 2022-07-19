65450084a7ecec98ba9bd5d64e16bb3902547b5c
Sonlowami <uwilowami64@outlook.com>
2022-07-19 13:13:43 -0700

    Add f(x) to find first occurance of a char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+/**
+ * _strchr - search for the first occurance of a character in a string
+ * @s: the string to look into
+ * @c:the character to look for
+ *
+ * Return: address of the first occurance
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	return (NULL);
+}
