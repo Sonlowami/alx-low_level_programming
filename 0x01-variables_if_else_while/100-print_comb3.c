6e6d4c54b459ca52f96aa36d8b593882e0b3ea42
Sonlowami <uwilowami64@outlook.com>
2022-07-23 07:38:48 -0700

    Change the printing logic to remove extra outputs

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,64 @@
 #include <stdio.h>
-
+#define IN 1
+#define OUT 0
 /**
- * main - combine unique numbers
+ * print_chars - print characters on the board
+ * @i: the tens
+ * @j: the ones
+ * @n: the needed combination of two digits
+ */
+void print_chars(int i, int j, int n)
+{
+
+	putchar(i + '0');
+	putchar(j + '0');
+	if (n != 89)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+/**
+ * main - print a combination of two unique digits
  *
  * Return: 0
  */
-
 int main(void)
 {
-	int i, j;
+	int x[200];
+	int i, j, filled, n1, n2;
+	int state = OUT;
 
+	filled = 0;
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
-			if (i != j)
+			int k;
+
+			n1 = i * 10 + j;
+			n2 = j * 10 + i;
+			for (k = 0; k <= filled && filled < 100; k++)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(',');
-				putchar(' ');
+				if (x[k] == n1 || x[k] == n2)
+				{
+					state = IN;
+					break;
+				}
+				else if (state == IN)
+					state = OUT;
+			}
+			if (state == IN)
+				continue;
+			if (state == OUT && i != j)
+			{
+				x[filled] = n1;
+				x[filled + 1] = n2;
+				filled += 2;
+				print_chars(i, j, n1);
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
