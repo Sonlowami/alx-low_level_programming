922495ba7e6ae4b2973e9868413623341671885b
Sonlowami <uwilowami64@outlook.com>
2022-07-26 10:16:32 -0700

    Remove a trailing spae at line 19

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size > 0)
 	{
 		char *str = (char *) malloc(sizeof(char) * size);
-		
+
 		if (str == NULL)
 			return (NULL);
 		for (i = 0; i < size; i++)
