39e3c29c180eaea99c735bad57338f88c1e74922
Sonlowami <uwilowami64@outlook.com>
2022-07-27 14:04:45 -0700

    Add a function to concatenate all the CL args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - concatenate all arguments to
+ * the program
+ * @ac: count of arguments
+ * @av: arrat of arguments
+ *
+ * Return: pointer to new string
+ */
+char *argstostr(int ac, char **av)
+{
+	unsigned long int size, j;
+	int i, k;
+	char *p;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	/*give size to p*/
+	size = 0;
+	for (i = 0; i < ac; i++)
+	{
+		size += strlen(av[i]);
+	}
+	p = (char *) malloc(sizeof(char) * (size + ac));
+
+	if (p)
+	{
+		i = 0;
+		for (k = 0 ; k < ac; k++)
+		{
+			for (j = 0; j < strlen(av[k]); j++)
+			{
+				p[i] = av[k][j];
+				i++;
+			}
+			p[i] = '\n';
+			i++;
+		}
+		return (p);
+	}
+	return (NULL);
+}
