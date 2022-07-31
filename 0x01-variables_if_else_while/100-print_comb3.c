cac319e4dace8e2e99335db7e126042ae02b4460
Sonlowami <uwilowami64@outlook.com>
2022-07-31 07:00:28 -0700

    Change logic of 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,4 @@
 #include <stdio.h>
-#define IN 1
-#define OUT 0
-/**
- * print_chars - print characters on the board
- * @i: the tens
- * @j: the ones
- * @n: the needed combination of two digits
- */
-void print_chars(int i, int j, int n)
-{
-
-	putchar(i + '0');
-	putchar(j + '0');
-	if (n != 89)
-	{
-		putchar(',');
-		putchar(' ');
-	}
-}
 /**
  * main - print a combination of two unique digits
  *
@@ -25,37 +6,18 @@ void print_chars(int i, int j, int n)
  */
 int main(void)
 {
-	double x[200];
-	int i, j, filled, n1, n2;
-	int state = OUT;
+	int i, j;
 
-	filled = 0;
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 1; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			int k;
-
-			n1 = i * 10 + j;
-			n2 = j * 10 + i;
-			for (k = 0; k <= filled && filled < 100; k++)
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i * 10 + j < 89)
 			{
-				if (x[k] == n1 || x[k] == n2)
-				{
-					state = IN;
-					break;
-				}
-				else if (state == IN)
-					state = OUT;
-			}
-			if (state == IN)
-				continue;
-			if (state == OUT && i != j)
-			{
-				x[filled] = n1;
-				x[filled + 1] = n2;
-				filled += 2;
-				print_chars(i, j, n1);
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
