f879f4fe871c55b12230b8d1ad5ab796a2e0212c
Sonlowami <uwilowami64@outlook.com>
2022-08-02 13:19:36 -0700

    Add dog.h struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+This directory contain programs that solve problems in project 0X0E.C C - Structures and typedef
+in the ALX - Software Engineering Program. 
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - properties of a dog group
+ * @name: name of the dog
+ * @age: how old the dog is
+ * @owner: the name of the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
