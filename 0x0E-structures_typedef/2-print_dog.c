74d9e19fb23a26c53f70b472cc32be2e27218263
Sonlowami <uwilowami64@outlook.com>
2022-08-02 14:18:41 -0700

    Add 2-print_dog.c to print items in a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print object of struct dog
+ * @d: the object to print
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		;
+	}
+	else
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+
+		else
+			printf("Name: nil\n");
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner != NULL)
+			printf("Name: %s\n", d->owner);
+
+		else
+			printf("Name: nil\n");
+	}
+}
