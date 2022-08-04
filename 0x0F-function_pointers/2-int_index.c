10672574f716eec19f92a1d7f1080afb966d82cf
Sonlowami <uwilowami64@outlook.com>
2022-08-04 12:39:28 -0700

    Add 2-int_index to search an int in an array of ints

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search an int in an array of ints
+ * @array: the array of ints to scrumble
+ * @size: the size oth array
+ * @cmp: the pointer to function that compare
+ *
+ * Return: index of the first match
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size == 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
