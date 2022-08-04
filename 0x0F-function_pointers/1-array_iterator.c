830b9f037196922d0c2a4c5fe20a65a47d01e42b
Sonlowami <uwilowami64@outlook.com>
2022-08-04 11:52:14 -0700

    Add f(x)that loops through an array, taking action
    The action taken by the array_iterator f(x) is specified in the
    function pointer passed to it at runtime.

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - loop through an array, taking
+ * actions specified in last parameter on each
+ * @array: the array of intsto process
+ * @size: the size of the array
+ * @action: pointer to a function that carries out the action
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
