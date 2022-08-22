bf1166c9ebd7a34a81bfdacc638816cd2a1873e1
Sonlowami <uwilowami64@outlook.com>
2022-08-22 03:05:40 -0700

    Handle Null double pointer head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node_a, *node_b;
 
-	node_a = *head;
-	while (node_a)
+	if (head != NULL)
 	{
-		node_b = node_a->next;
-		free(node_a);
-		node_a = NULL;
-		node_a = node_b;
-	}
+		node_a = *head;
+		while (node_a)
+		{
+			node_b = node_a->next;
+			free(node_a);
+			node_a = NULL;
+			node_a = node_b;
+		}
 	*head = NULL;
+	}
 }
