1b6f1eb05d69dce9fea69b6e8f406b320754fffc
Sonlowami <uwilowami64@outlook.com>
2022-10-08 23:16:06 +0200

    Correct wget arguments to put the output in /tmp/

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/Sonlowami/alx-low_level_programming/tree/main/0x18-dynamic_libraries/override.so
-export LD_PRELOAD='./override.so:$LD_PRELOAD'
+wget -qP /tmp/ https://github.com/Sonlowami/alx-low_level_programming/tree/main/0x18-dynamic_libraries/override.so
+export LD_PRELOAD='/tmp/override.so'
