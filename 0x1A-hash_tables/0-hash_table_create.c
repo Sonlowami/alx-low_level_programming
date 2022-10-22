5f782d22c10968a3ef59a5f641e271b264ff0423
Sonlowami <uwilowami64@outlook.com>
2022-10-22 22:24:02 +0200

    Adjust the size of the hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
-	new = malloc(sizeof(hash_table_t *));
+	new = malloc(sizeof(hash_table_t *) + 1);
 	if (!new)
 		return (NULL);
 	if (size > 0)
